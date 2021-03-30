365891f228cb869212449d6ebd8043bc603fbed9
Lee Jones <lee@lees-mbp.lan>
2021-03-30 17:30:22 +0100

    added some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
-  get '/', to: "posts#index"
+  
+  root to: "public#homepage"
+
+  get '/posts', to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
